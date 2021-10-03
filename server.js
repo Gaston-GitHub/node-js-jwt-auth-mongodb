fc490fd3bdb5e61abdc3c6c52e6b2f985d2c6b07
Gaston <gaston@github.com>
2021-10-03 19:39:50 +0200

    created setup express web server

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 // Setup Express web server 
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require('express'); // express is for building the Rest apis
+
+const cors = require('cors'); // provides Express middleware toenable CORS
 
 const app = express();
 
-let corsOptions = {
+let corseOptions = {
     origin: 'https://localhost:8081'
 };
 
 app.use(cors(corseOptions));
 
-app.use(bodyParser.json()); // parse request of content type - application/JSON
+app.use(express.json()); // parse request of content type - application/JSON
 
-app.use(bodyParser.urlencoded({ extender: true })); // parse request of content type
+app.use(express.urlencoded({ extender: true })); // parse request of content type
 
-app.get('/', (req, res) => )
+//simple route
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to my app' });
+});
+
+// set port, listen for request
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
