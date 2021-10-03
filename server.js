d3a23f07d66e39c9e59492390c97dea635d1b93b
Gaston <gaston@github.com>
2021-10-03 21:45:31 +0200

    added connection Mongoose to MongoDb

--- server.js
+++ server.js
@@ -10,6 +10,23 @@ let corseOptions = {
     origin: 'https://localhost:8081'
 };
 
+const db = require('./app/models');
+const Role = db.role;
+
+db.mongoose
+    .connect(`mongodb://${db.Config.HOST}:${dbConfig.PORT}/${db.Config.DB}`, {
+        userNewUrlParser: true,
+        userUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Successfully connected to MongoDB.');
+        initial();
+    })
+    .catch(err => {
+        console.error('Connection error', err)
+        process.exit()
+    })
+
 app.use(cors(corseOptions));
 
 app.use(express.json()); // parse request of content type - application/JSON
@@ -26,3 +43,37 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+
+// initial functions help to create 3 importante rows in roles collection
+function initial() {
+    Role.estimatedDocumentCount((err, couunt) => {
+        if(!err && count === 0) {
+            new Role({
+                name: 'user'
+            }).save(err => {
+                if(err) {
+                    console.log('error', err)
+                }
+                console.log("added 'user' to roles collection");
+            });
+
+            new Role({
+                name: 'moderator'
+            }).save(err => {
+                if(err) {
+                    console.log('error', err);
+                }
+                console.log("added 'moderator' to roles collection");
+            });
+
+            new Role({
+                name: 'admin',
+            }).save(err => {
+                if(err) {
+                    console.log('error', err)
+                }
+                console.log("added 'admin' to roles collection")
+            });
+        }
+    });
+}
