d5dbe5d48e7774fc38d8032230c7712d4eb036c6
Gaston <gaston@github.com>
2021-10-09 12:49:25 +0200

    updated server with auth user routes

--- server.js
+++ server.js
@@ -38,6 +38,11 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to my app' });
 });
 
+//routes
+
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 // set port, listen for request
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
